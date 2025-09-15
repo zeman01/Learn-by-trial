43dd2203fe89218fa45131fbef647bbe522db364
zeman01 <deshadow95@gmail.com>
2025-09-15 21:27:51 +0545

    add program to convert temperature

--- tempConvert.js
+++ tempConvert.js
@@ -1,11 +1,19 @@
+
+// ! Function to convert celsius to fehrenheit
 function celsiusToFehrenheit(celsius) {
   let temp = (celsius * 9) / 5 + 32;
   console.log(temp);
 }
 
+
+// ! Function to convert fehrenheit to celsius
+
 function fehrenheitToCelsius(fehrenheit) {
   let temp = ((fehrenheit - 32) * 5) / 9;
   console.log(temp);
 }
+
+// call function celsiusToFehrenheit
 celsiusToFehrenheit(-5);
+// call functiom fehrenheitToCelsius
 fehrenheitToCelsius(86);
